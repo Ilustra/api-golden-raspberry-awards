3026391f460e25a42273d7ea17980c2a9ed652a1
Andrade kaique <kaique.andrade@consulting-for.edenred.com>
2025-07-09 18:26:07 -0300

    fix: rename path

--- src/app/controllers/movie/movieController.js
+++ src/app/controllers/movie/movieController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Movie = require('../../models/movie');
-const { findAll, findWinner } = require('../../service/MovieService');
+const { findAll, findWinner } = require('../../service/movieService');
 
 
 
